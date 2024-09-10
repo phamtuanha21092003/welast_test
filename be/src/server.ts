c698125c7d77a1070e5dbe72364ee90570bf06df
Pham Ha <phamtuanha21092003@gmail.com>
2024-09-10 12:19:29 +0700

    api filter

--- be/src/server.ts
+++ be/src/server.ts
@@ -11,6 +11,7 @@ app.use(cors())
 interface Repo {
     fork: boolean
     forks: number
+    language: string | null
 }
 
 app.get("/repos", async (req: Request, res: Response) => {
@@ -24,6 +25,28 @@ app.get("/repos", async (req: Request, res: Response) => {
     res.setHeader("Content-Type", "application/json")
     res.json({
         data: repositories,
+    })
+})
+
+app.get("/filters", async (req: Request, res: Response) => {
+    const response: AxiosResponse<any> = await axios.get(
+        "https://api.github.com/users/freeCodeCamp/repos"
+    )
+
+    const languages: Set<string | null> = response.data?.reduce(
+        (accumulator: Set<string | null>, repo: Repo) => {
+            if (accumulator.has(repo.language)) {
+                return accumulator
+            }
+
+            accumulator.add(repo.language)
+            return accumulator
+        },
+        new Set<string | null>()
+    )
+
+    res.json({
+        data: Array.from(languages),
     })
 })
 
